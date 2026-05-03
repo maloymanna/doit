d0b00c2df929f1eba2c9908a95dd1e393a915aee
maloymanna <maloymanna@hotmail.com>
2026-05-04 01:57:32 +0200

    Q Ph1 - fix for windows importerror

--- src/doit/plugins/__init__.py
+++ src/doit/plugins/__init__.py
@@ -1,3 +1,2 @@
-# src/doit/plugins/__init__.py [MOD v1.1]
-# Kept empty to avoid Windows namespace/caching resolution issues.
-# Submodules are imported directly in action_dispatcher.py
+# src/doit/plugins/__init__.py [MOD v1.2]
+"""Doit plugin registry namespace."""
